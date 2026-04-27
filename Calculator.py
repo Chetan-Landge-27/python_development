5fa950515d50c1c6510c38521044b995ea0209bd
Chetan-Landge-27 <landgechetan27@gmail.com>
2026-04-27 14:48:47 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,13 +1,11 @@
 def get_input():
     try:
         num1 = float(input("Enter first number: "))
-        num2 = float(input("Second first number: "))
+        num2 = float(input("Enter second number: "))
         return num1, num2
     except ValueError:
         print("Invalid input. Please enter numbers only.")
         return None, None
-
-
 
 def add(a, b):
     return a + b 
@@ -23,34 +21,37 @@ def divide(a, b):
         return "Error: Division by Zero!"
     return a / b
 
-
 def calculator():
-    print("Select operation:")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
+    while True:   
+        print("\nSelect operation:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
 
-    choice = input("Enter choice (1/2/3/4): ")
-    if choice not in['1', '2', '3', '4']:
-        print("Invalid choice. please select 1, 2, 3, or 4.")
-        return
+        choice = input("Enter choice (1/2/3/4/5): ")
 
-    num1, num2 = get_input()
-    if num1 is None or num2 is None:
-        return
-    if choice == '1':
-        print("Result:", add(num1, num2))
-    elif choice == '2':
-        print("Result:", subtract(num1, num2))
-    elif choice == '3':
-        print("Result:", multiply(num1, num2))
-    elif choice == '4':
-        print("Result:", divide(num1, num2))
-    else:
-        print("Invalid choice")
+        if choice == '5':
+            print("Exiting...!")
+            break
 
+        if choice not in ['1', '2', '3', '4']:
+            print("Invalid choice. Please select 1, 2, 3, 4, or 5.")
+            continue
+
+        num1, num2 = get_input()
+        if num1 is None or num2 is None:
+            continue
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
 
 if __name__ == "__main__":
-
     calculator()
